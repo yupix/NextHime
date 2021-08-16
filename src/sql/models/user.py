1b8c85b890efd8fb86b7f1f813c3372b08fc0e4a
yupix <yupi0982@outlook.jp>
2021-08-17 07:16:17 +0900

    feat: ユーザーモデルを追加 #30

---
+++ src/sql/models/user.py
@@ -0,0 +1,8 @@
+from sqlalchemy import BIGINT, Column, String
+from NextHime import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    user_id = Column(BIGINT, primary_key=True)
+    locale = Column(String)
