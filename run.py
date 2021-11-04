b0b096ee944ec45bc13aa5295b63c29941ef8157
yupix <yupi0982@outlook.jp>
2021-11-04 17:37:47 +0900

    chore: importの順番を変更

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import asyncio
+
+from NextHime.main import run
 from src.modules.color import Color
 
 with open('src/assets/logo.txt') as logo:
     print(Color().pink + logo.read())
 
-from NextHime.main import run
 
 if __name__ == "__main__":
     loop_bot = asyncio.new_event_loop()
